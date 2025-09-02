aa6b633d55b0dcc1d12fd12be8bd7ee43e6216b5
Samuel <samuelgomes101112@gmail.com>
2025-09-01 22:05:13 -0300

    add

---
+++ src/views/home.vue
@@ -0,0 +1,24 @@
+<template>
+
+    <div v-for="movie in movies" :key="movie.id" class="flex column justify-center mt-5">
+        <p>{{ movie.title }}</p>
+    </div>
+
+</template>
+
+<script setup>
+
+import axios from "axios";
+import { ref } from "vue";
+
+const movies = ref()
+
+axios.get('movies').then((response) => {
+    //  console.log(response.data) 
+
+    movies.value = response.data
+
+})
+
+
+</script>
