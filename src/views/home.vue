bab43a2531b02c31d9a08e8f989a43b1fe47245f
Samuel <samuelgomes101112@gmail.com>
2025-09-02 22:46:09 -0300

    update the method patch and delete, with axios

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,7 +1,20 @@
 <template>
 
     <div v-for="movie in movies" :key="movie.id" class="flex column justify-center mt-5">
-        <p>{{ movie.title }}</p>
+
+        <div class="flex flex-col gap-2">
+            <div class="flex border p-2 pl-4 pr-8 rouded">
+                <button @click="updateMovie(movie)">
+                    <div v-if="movie.completed" class="border rounded-full p-3 mx-2 bg-green-500"> </div>
+                    <div v-else class="border rounded-full p-3 mx-2 bg-blue-100"> </div>
+                </button>
+                {{ movie.title }}
+                <button class="ml-4 px-2 rounded bg-red-800 text-sm" @click="deleteMovie(movie.id)">Delete</button>
+            </div>
+
+
+
+        </div>
     </div>
 
 </template>
@@ -14,11 +27,37 @@ import { ref } from "vue";
 const movies = ref()
 
 axios.get('movies').then((response) => {
-    //  console.log(response.data) 
 
     movies.value = response.data
 
 })
+
+
+function updateMovie(movie) {
+
+    const data = {
+        completed: !movie.completed
+    }
+
+    axios.patch(`movies/${movie.id}`, data).then((response) => {
+
+        movie.completed = response.data.completed
+
+    })
+
+}
+
+function deleteMovie(id) {
+
+
+    axios.delete(`movies/${id}`).then((response) => {
+
+        console.log(response);
+
+    });
+
+}
+
 
 
 </script>
